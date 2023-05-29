79328fea308b9e565ecc1d245663121f6dd8a7ba
CHELVYN01 <kledenkelvin9@gmail.com>
2023-05-29 11:11:42 +0700

    feat:memebuat product display

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <!-- <ProductShop /> -->
-    <CobaShop />
+    <ProductShop />
+    <!-- <CobaShop /> -->
   </div>
 </template>
 
 <script>
-// import ProductShop from "./components/ProductShop.vue";
-import CobaShop from "./components/CobaShop.vue";
+import ProductShop from "./components/ProductShop.vue";
+// import CobaShop from "./components/CobaShop.vue";
 
 export default {
   name: "App",
   components: {
-    // ProductShop,
-    CobaShop,
+    ProductShop,
+    // CobaShop,
   },
 };
 </script>
--- src/components/ProductShop.vue
+++ src/components/ProductShop.vue
@@ -4,7 +4,12 @@ export default {
   data() {
     return {
       data: null,
+      productIndex: 0,
+      visibleProducts: [],
     };
+  },
+  mounted() {
+    this.fetchData();
   },
   methods: {
     async fetchData() {
@@ -13,8 +18,43 @@ export default {
         const data = await response.json();
         this.data = data;
         console.log(data);
+        this.filterProducts();
       } catch (error) {
         console.error(error);
+      }
+    },
+    filterProducts() {
+      if (this.data) {
+        this.visibleProducts = this.data.filter(
+          (product) =>
+            product.category === "men's clothing" ||
+            product.category === "women's clothing"
+        );
+      }
+    },
+    nextProduct() {
+      if (this.productIndex < this.visibleProducts.length - 1) {
+        this.productIndex++;
+      }
+    },
+  },
+  computed: {
+    currentProduct() {
+      return this.visibleProducts[this.productIndex];
+    },
+    roundRating() {
+      if (this.currentProduct && this.currentProduct.rating) {
+        const rating = this.currentProduct.rating.rate;
+        const fullRound = Math.floor(rating);
+        const halfRound = rating % 1 !== 0;
+        const emptyRound = 5 - Math.ceil(rating);
+        return {
+          fullRound: fullRound,
+          halfRound: halfRound,
+          emptyRound: emptyRound,
+        };
+      } else {
+        return null;
       }
     },
   },
@@ -22,22 +62,57 @@ export default {
 </script>
 <template>
   <div>
-    <section class="Containerbg-Pattern">
-      <div class="bg-Pattern">
-        <img src="../assets/img/bg-pattern.svg" alt="" />
-      </div>
-      <div>
-        <button @click="fetchData">Ambil Data</button>
-        <div v-if="data">
-          <ul>
-            <li v-for="product in data" :key="product.id">
-              {{ product.title }}
-            </li>
-          </ul>
+    <section v-if="data">
+      <div
+        v-for="product in visibleProducts"
+        :key="product.id"
+        v-show="productIndex === visibleProducts.indexOf(product)"
+        :class="{
+          'women-clothing': product.category === 'women\'s clothing',
+          'men-clothing': product.category === 'men\'s clothing',
+        }"
+      >
+        <div class="bg">
+          <img
+            :src="product.image"
+            :alt="product.title"
+            class="product-image"
+          />
+          <div class="product">
+            <h2 class="product-title">{{ product.title }}</h2>
+            <div class="information">
+              <h3 class="product-category">{{ product.category }}</h3>
+              <div class="rating">
+                <h2 class="rate">{{ product.rating.rate }}/5</h2>
+                <template v-if="roundRating">
+                  <template
+                    v-for="(item, index) in roundRating.fullRound"
+                    :key="index"
+                  >
+                    <span class="round-icon"></span>
+                  </template>
+                  <span v-if="roundRating.halfRound" class="round-icon"></span>
+                  <template
+                    v-for="(item, index) in roundRating.emptyRound"
+                    :key="index"
+                  >
+                    <span class="round-icon empty"></span>
+                  </template>
+                </template>
+              </div>
+            </div>
+            <hr />
+            <br />
+            <p class="product-description">{{ product.description }}</p>
+
+            <hr class="line" />
+            <h2 class="product-price">{{ product.price }}</h2>
+            <div class="button-pruduct">
+              <button @click="nextProduct" class="next-button">next</button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
   </div>
 </template>
-
-<style></style>
